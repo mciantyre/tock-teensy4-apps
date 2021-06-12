c55ced5cc29459f80f94e9f887369a11c35ce988
Ian McIntyre <ianpmcintyre@gmail.com>
2021-06-12 13:20:03 -0400

    Squashed 'tock/' changes from 60a9eb15b..306d3ee3c
    306d3ee3c teensy40: Use DMA transfers for LPUART2
    f2119e6dd imxrt10xx: Integrate DMA into LPUART
    50bde3b30 fixup! imxrt10xx: Add the DMA peripheral
    c42dabe6d imxrt10xx: Add the DMA peripheral
    8bf3a98a0 Merge #2580 #2585 #2607 #2608 #2611 #2616
    678649b24 Merge #2612
    baaa3551c Merge #2620
    a7fa081ac rename Platform structure to MicroBit
    65a53b2c8 Merge #2587
    fba435b38 Merge #2598
    73f240814 Apply suggestions from Pat review
    6347e2000 Fix a bug in the SPI controller capsule.
    33facadc8 Merge #2606
    6a86ffcaa Merge #2586
    3c3fbc749 Merge #2609
    03988df7d imxrt1050-evkb: update README flashing instructions
    fc6c4865f imxrt1050-evkb/Makefile: remove path assumptions & refactor
    67fa2451d Merge #2613
    32bc6b7f4 fix Yaffs1 link in tickv README
    5149d298e Merge #2582
    c3034cd5b capsules: i2c_master: Fix grant double enter
    0bb62668e nordic/nrf52840dk: Connect the I2C device
    dadeb691d pconsole: comments and cleanup
    79b52e580 update new Process functions
    87327bad6 revert introspection
    51a61af78 update process console
    0e3f42e30 boards: remove added whitespace
    8836a24bc Merge pull request #2605 from hudson-ayers/tock-size-tool-riscv
    8cba3dd0e opentitan: README: Update the QEMU requirement
    27e23c004 Merge #2596
    3d7857bb2 shell.nix: add cxxfilt Python package
    2abc9e486 Removed Space
    450652632 Merge branch 'trd-hil-update' of github.com:tock/tock into trd-hil-update
    e5bc1a963 Addresses.
    5a19ea3be Minor fixes to removing driver macro
    25be42db9 removed drivers macro
    57b0097ce Merge #2604
    46f8bde2a update touch driver to return i2c buffers
    7ddd34ce8 update apds9960 driver to return i2c buffer
    222d5475e verify i2c errors for bus and st77xx
    cdb270bf6 Update doc/wg/core/notes/core-notes-2021-05-28.md
    9c381dc83 Update doc/wg/core/notes/core-notes-2021-05-28.md
    a1b6fe125 build with codegen-units = 1 to reduce code size
    8bb3ac017 allow size tool to be used on non-arm architectures
    05f2360b7 Add notes from core meeting 2021-05-28
    51214b89d rv32i: document why memory-reordering can't affect switch_to_process
    d019e11b2 Apply suggestions from code review
    57793f5ee Add Brad to author list.
    4871770ca Add 3 more rules as per Brad's suggestion.
    1b2dee221 fence off the whole RUSTUP block
    566011258 doc: trd-hil: add tips from tock-book
    401ff08d5 boards/Makefile.common: inform about ability to set NO_RUSTUP
    aa7a202d1 shell.nix: set NO_RUSTUP environment variable
    3b4fda7e3 opentitan: plic: Remove unused code
    215a7b797 opentitan: Claim the HMAC interrupt before we handle it
    4efd594e2 lowrisc: hmac: Don't call add_data_done upcall syncronously
    ca2784b30 capsules: hmac: Use the correct destination buffer
    ef803aa2a capsules: hmac: Don't double enter grants
    3297e1f87 digest: Use const generics for the digest array
    ca5b79823 hmac: Add support for different HMAC types
    63c911725 hmac: Make the key length varialbe
    43470f2db allow rustup opt-out
    d509dd160 boards: make: check for rustup
    c49bb6fcf made driver struct data optional
    d320622ce Fixed merge issues
    a877f8489 Merge branch 'master' into console_dev
    3e44bf692 microbit_v2/Makefile: remove unused variables
    1532dfa91 teensy40/Makefile: don't assume relative libtock-c path for app-tbf
    d306515d3 Reformated files
    2c06bb050 Added comments to explain state machine
    8ea21c1ba made final functional improvments, driver info printed with kernel command
    dc37dd229 Got drivers to print kind of
    49deabe9c minor improvment; added kernel version to console.
    16673e431 Improved console to use less ram(600 Bytes)
    27253220d Got Kernel Memory print out working
    2a26e2eab fixed formatting
    da7661e5f I think I got kernel and process commands fully running but output is a bit funky
    719b8de05 kernel implemented and functional
    1af17955f significant improvment, still not done with milestone 1 but much better
    bd95c55a6 Added printing queue and writer for process console. Still debugging
    
    git-subtree-dir: tock
    git-subtree-split: 306d3ee3cd3dd868c03c4c4e6503567731757fb8

--- shell.nix
+++ shell.nix
@@ -56,9 +56,17 @@ in
       rust_build
       llvm
       qemu
+
+      # Required for tools/print_tock_memory_usage.py
+      pythonPackages.cxxfilt
     ];
 
     LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
+
+    # Instruct the Tock gnumake-based build system to not check for
+    # rustup and assume all required tools are installed and available
+    # in the $PATH
+    NO_RUSTUP = "1";
 
     # The defaults "objcopy" and "objdump" are wrong (for x86), use
     # "llvm-obj{copy,dump}" as defined in the makefile
